73c97889193879bcd4268a60e3a65ead8d5f907d
fedorsimakov <fedor.simakov.a@gmail.com>
2021-07-29 10:16:11 +0500

    completed the fourth stage of the project

--- src/Cli.php
+++ src/Cli.php
@@ -5,8 +5,9 @@ namespace Brain\Games\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function run() {
-	line("Welcome to the Brain Games!");
-	$name = prompt('May I have your name?');
-	line("Hello, %s!", $name);
+function run()
+{
+    line("Welcome to the Brain Games!");
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }
