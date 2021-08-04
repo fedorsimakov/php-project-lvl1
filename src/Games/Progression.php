30e0f2857145dc0e2ee4ef17cdfbce66aced6bb7
fedorsimakov <fedor.simakov.a@gmail.com>
2021-08-04 17:18:36 +0500

    fixed line with progression

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -8,19 +8,18 @@ function game(string $name): int
 {
     $correctAnswer = null;
     $progression = [];
-    $progressionString = '';
     $a1 = rand(1, 20);
     $d = rand(1, 20);
     $emptyPosition = rand(0, 4);
     for ($i = 0; $i < 10; $i++) {
-        $progression[$i] = $a1 + $i * $d;
         if ($i === $emptyPosition) {
-            $progressionString = "{$progressionString} ..";
-        } else {
-            $progressionString = "{$progressionString} {$progression[$i]}";
+            $correctAnswer = $a1 + $i * $d;
+            $progression[$i] = "..";
+        } else {            
+            $progression[$i] = $a1 + $i * $d;
         }
     }
-    $correctAnswer = $progression[$emptyPosition];
+    $progressionString = implode(' ', $progression);
     line("Question: {$progressionString}");
     return $correctAnswer;
 }
